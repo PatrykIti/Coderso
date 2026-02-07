45f976e7f2f1b6e9e7e14e8544615e2ae6784ea6
Patryk Ciechanski <patryk.ciechanski@inetum.com>
2026-02-07 16:54:11 +0100

    chore: up

--- core/db/migrations/0032_widget_template_settings.sql
+++ core/db/migrations/0032_widget_template_settings.sql
@@ -1,3 +1,10 @@
+ALTER TABLE "content_term_assignments"
+DROP CONSTRAINT "content_term_assignments_pk";
+--> statement-breakpoint
+ALTER TABLE "content_term_assignments"
+ADD CONSTRAINT "content_term_assignments_entry_id_term_id_pk"
+PRIMARY KEY("entry_id","term_id");
+--> statement-breakpoint
 ALTER TABLE "widget_templates"
 ADD COLUMN "settings" jsonb NOT NULL DEFAULT '{}'::jsonb;
 --> statement-breakpoint
